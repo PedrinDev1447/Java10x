d8a2579ad25a907e68476503fac7bd4d9d60d78c
PEDRO LUCAS <pedrolucas.nft2021@gmail.com>
2025-08-05 20:44:25 -0300

    Desafio Java10x

--- 2PrimeiroDesafio/src/estruturaDeDados/Array.java
+++ 2PrimeiroDesafio/src/estruturaDeDados/Array.java
@@ -2,29 +2,50 @@ package estruturaDeDados;
 
 public class Array {
     public static void main(String[] args) {
-        //arrys sao tipos referencia
+        //arrays sao tipos referencia
 
-        String[] ninja = new String[5];
+        String[] ninja = new String[6];
         ninja[0] = "Naurto Uzumaki";
         ninja[1] = "Sasuke Uchiha";
         ninja[2] = "Sakura Haruno";
         ninja[3] = "Hinata Hyuga";
         ninja[4] = "Kakashi Hatake";
 
-        System.out.println("Nome do ninja: " + ninja[2]);
+       // System.out.println("Nome do ninja: " + ninja[2]);
+
+        //Redeclarar
+        ninja =  new String[7];
+        ninja[0] = "Hashirama Senju";
+        ninja[1] = "Tobirama Senju";
+        ninja[2] = "Hiruzen Sarutobi";
+        ninja[3] = "Minato Namikaze";
+        ninja[4] = "Tsunade";
+        ninja[5] = "Kakashi Hatake";
+        ninja[6] = "Naruto Uzuamaki";
+       // System.out.println("Nome do ninja: " + ninja[2]);
+
+        //For para fazer um LOOP no array
+        for (int i = 0; i < 7; i++) {
+            System.out.println(ninja[i]);
+        }
+
+
+
         //array idade
         int[] idade = new int[2];
         idade[0] = 16;
         idade[1] = 13;
-        System.out.println("Idade do ninja: " + idade[0]);
+        //System.out.println("Idade do ninja: " + idade[0]);
+
 
         boolean[] trueOrFlase = new boolean[1];
         trueOrFlase[0]= false;
-        System.out.println("Passou para proxima missao? " + trueOrFlase[0]);
+        //System.out.println("Passou para proxima missao? " + trueOrFlase[0]);
 
         double[] flutuante = new double[2];
         flutuante[0] = 0.1;
-        System.out.println("Pontos da missão: " +  flutuante[0]);
+        //System.out.println("Pontos da missão: " +  flutuante[0]);
+
 
 
 
--- src/Desafios/NivelIntermediario.java
+++ src/Desafios/NivelIntermediario.java
@@ -1,4 +1,68 @@
 package Desafios;
 
+import java.util.Scanner;
+
 public class NivelIntermediario {
+    public static void main(String[] args) {
+
+        //Entrada de dados
+        Scanner scanner = new Scanner(System.in);
+
+        //Fazer o array
+        int NUMBER_MAX = 10;
+        String[] ninja = new String[NUMBER_MAX];
+
+
+
+        //Contadores
+
+        int ninjasRegister = 0;
+        int option = 0;
+
+        while (option != 3) {
+
+            // Menu
+            System.out.println("\n===== Menu Ninja =====");
+            System.out.println("1. Cadastrar Ninja");
+            System.out.println("2. Listar Ninjas");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            option = scanner.nextInt();
+
+            switch (option){
+                case 1:
+                    if (ninjasRegister < NUMBER_MAX){
+                        System.out.println("Digiste o nome do ninja para cadastro: ");
+                        String nameNinjas = scanner.nextLine();
+                        ninja[ninjasRegister] = nameNinjas;
+                        ninjasRegister++;
+                        System.out.println("Ninja cadastrado com sucesso!");
+                    }else  {
+                        System.out.println("A lista de ninjas está cheia, impossivel cadastrar um novo ninja.");
+                    }
+                    break;
+                case 2:
+                    if (ninjasRegister == 0){
+                        System.out.println("Nenhum ninja foi encontrado ");
+                    }else {
+                        System.out.println("\n===== Lista de Ninjas =====");
+                        for (int i = 0; i < ninja.length; i++) {
+                            System.out.println(ninja[i]);
+                        }
+                    }
+                    break;
+                case 3:
+                        System.out.println("Obrigado pelo cadastro! Estamos terminando o programa...Aguarde");
+                        break;
+                default:
+                    System.out.println("Esta opção não é valida");
+                    break;
+
+
+
+
+            }
+        }
+
+    }
 }
