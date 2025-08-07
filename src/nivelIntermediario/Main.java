6fa3dbf803c7fa4e8e19720097c2cf357ce1ae09
PEDRO LUCAS <pedrolucas.nft2021@gmail.com>
2025-08-07 20:52:25 -0300

    Iniciando o módulo intermediario com POO

---
+++ src/nivelIntermediario/Main.java
@@ -0,0 +1,28 @@
+package nivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+        //Criar Naurto Uzumaki - Naruto é um objeto
+
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.aldeia = "Aldeia da folha";
+        Naruto.idade = 17;
+
+
+        //Criar Sasuke Uchiha - Sasuke é um objeto
+
+        Ninja Sasuke = new Ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.aldeia = "Aldeia da folha";
+        Sasuke.idade = 18;
+
+        //Criar Sakura Haruno - Saruka é um objeto
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.aldeia = "Aldeia da folha";
+        Sakura.idade = 18;
+
+
+    }
+}
---
+++ src/nivelIntermediario/Ninjas.java
@@ -0,0 +1,8 @@
+package nivelIntermediario;
+
+public class Ninja {
+    String nome ;
+    String aldeia;
+    int idade;
+
+}
