804e6dff675d8400d2bc9d3f93e6e2533b299b53
PEDRO LUCAS <pedrolucas.nft2021@gmail.com>
2025-08-09 22:59:54 -0300

    ínicio do estudo a herança

--- src/nivelIntermediario/Ninjas.java
+++ src/nivelIntermediario/Ninjas.java
@@ -1,31 +1,7 @@
 package nivelIntermediario;
 
 public class Ninjas {
-    public String nome;
-    public String aldeia;
-    public int idade;
-
-    //O método VOID nao retorna método nenhum
-    public void SharinganAtivado() {
-        System.out.println("O Sharingan foi ativado");
-    }
-
-    @Override
-    public String toString() {
-        return "Ninja: " + nome +
-                " | Aldeia: " + aldeia +
-                " | Idade: " + idade;
-    }
-    /*
-    * O método String tem que retornar uma String
-    * */
-
-    public String EuSouUmNinja(){
-        return "Hi, I'm Ninja";
-
-    }
-
-    public int anosParaSeTornarHokage(int idadeMinimaParaSeTornarHokage){
-        return idadeMinimaParaSeTornarHokage - idade ;
-    }
+    //Objeto 1
+    Uchiha Sasuke = new Uchiha();
+    Sasuke.
 }
---
+++ src/nivelIntermediario/Uchiha.java
@@ -0,0 +1,11 @@
+package nivelIntermediario;
+
+public class Uchiha extends Ninjas{
+
+
+    //Sharingan ativado, método publico
+    public void SharinganAtivado() {
+        System.out.println("Meu nome é " + nome + ". O meu Sharingan foi ativado, eu sou um Uchiha!");
+    }
+
+}
