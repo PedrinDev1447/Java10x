b6b26e305d4bed5c3feb139dd78717a3b595e040
PEDRO LUCAS <pedrolucas.nft2021@gmail.com>
2025-08-14 21:36:50 -0300

    Fiz a implementação de polimorfismo ao código

---
+++ src/Polimorfismo/Ninja.java
@@ -0,0 +1,13 @@
+package Polimorfismo;
+
+public class Ninja {
+
+    String nome;
+    String aldeia;
+    int idade;
+
+    //Método geral! Todos os ninjas irão ter
+    public void habilidadeEspecial() {
+        System.out.println("Meu nome é " + nome + "e esse é meu ataque especial!" );
+    }
+}
