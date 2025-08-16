a06db2de9294ab6923320356b0ae9f02de780fba
PEDRO LUCAS <pedrolucas.nft2021@gmail.com>
2025-08-15 21:41:44 -0300

    Estudo do metodo constructors

---
+++ src/MetodoConstrutor/Hokages.java
@@ -0,0 +1,17 @@
+package MetodoConstrutor;
+
+public class Hokages {
+    String nome;
+    int idade;
+    boolean vivoOuNao;
+
+
+    public Hokages() {
+    }
+
+    public Hokages(String nome, int idade, boolean vivoOuNao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+    }
+}
---
+++ src/MetodoConstrutor/Main.java
@@ -0,0 +1,11 @@
+package MetodoConstrutor;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
